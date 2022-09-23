c7bb4270455781b7d51b454799716030a275e51a
yohelperez <perezyohel@gmail.com>
2022-09-22 23:19:15 -0500

    Adding to checkout functioning correctly

--- store/views.py
+++ store/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse 
+import json
+
 from .models import * 
+
 
 # Create your views here.
 
@@ -33,3 +37,32 @@ def checkout(request):
     
     context = {'items': items, 'order': order }
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    print("funciton started")
+    data = json.loads(request.body) 
+    productId = data['productId']
+    action = data['action']
+    
+    #shows in console t
+    print( 'Action:', action )
+    print( 'productId:', productId   )
+    print('checking')
+    
+    customer = request.user.customer 
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+    
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':  
+        orderItem.quantity = (orderItem.quantity - 1)
+        
+    orderItem.save()
+    
+    if orderItem.quantity <= 0 :
+        orderItem.delete()
+        
+    return JsonResponse('Item was added', safe=False) 
