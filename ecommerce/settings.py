cc56af06382f9d8a28f1695cbde16355a7a1c583
yohelperez <perezyohel@gmail.com>
2022-10-19 23:37:28 -0500

    heroku deployement

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-4kfg^)f)kn7&@yd5h4z9o_8@u2qh46+46w&$=)w9e8a2tah#$2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
@@ -133,5 +136,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images') #folder to save pictures
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+django_heroku.settings(locals())
 
 
