8c5ec8204b825749354a4440c31931e5d2d5dedd
yohelperez <perezyohel@gmail.com>
2022-10-21 00:00:00 -0500

    correction in code

--- store/models.py
+++ store/models.py
@@ -1,5 +1,5 @@
 from itertools import product
-from msilib.schema import Class
+#from msilib.schema import Class
 from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import User #django default user model
